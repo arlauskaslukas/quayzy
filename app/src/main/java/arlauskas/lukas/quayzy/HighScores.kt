5c542e1435135059d14d37251e7c76232bb63fde
unknown <arlauskaslukas@gmail.com>
2020-12-16 03:22:36 +0200

    ready for publishing

--- app/src/main/java/arlauskas/lukas/quayzy/HighScores.kt
+++ app/src/main/java/arlauskas/lukas/quayzy/HighScores.kt
@@ -1,8 +1,11 @@
 package arlauskas.lukas.quayzy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.ListView
 import retrofit2.Call
 import retrofit2.Callback
@@ -21,6 +24,13 @@ class HighScores : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_high_scores)
         userslist = retrieveList()
+        val button = findViewById<Button>(R.id.button13)
+        button.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0: View?) {
+                val intent = Intent(this@HighScores, Menu::class.java)
+                startActivity(intent)
+            }
+        })
     }
     fun retrieveList() : MutableList<UserScores>
     {
